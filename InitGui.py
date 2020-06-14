b1a0d06af93c646625ad55f88d4cc9f048a56f32
Zolko-123 <zolko@zzaero.com>
2020-06-14 23:37:13 +0200

    added Measure tool and BoM (Part List)
    still in development

--- InitGui.py
+++ InitGui.py
@@ -3,8 +3,6 @@
 #
 #  InitGui.py
 #  
-#  Copyright 2018 Mark Ganson <TheMarkster> mwganson at gmail
-#  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
@@ -23,6 +21,8 @@
 #  
 ###################################################################################
 
+
+import os
 
 import asm4wb_locator
 asm4wbPath = os.path.dirname( asm4wb_locator.__file__ )
@@ -62,26 +62,34 @@ class dropDownCmdGroup:
     +-----------------------------------------------+
 """
 class Assembly4Workbench(Workbench):
- 
-    global main_Assembly4WB_Icon
 
+    global main_Assembly4WB_Icon
     MenuText = "Assembly 4"
     ToolTip = "Assembly 4 workbench"
     Icon = main_Assembly4WB_Icon
 
-    
     def __init__(self):
         "This function is executed when FreeCAD starts"
         pass
+
+    def Activated(self):
+        "This function is executed when the workbench is activated"
+        return
+
+    def Deactivated(self):
+        "This function is executed when the workbench is deactivated"
+        return 
+
+    def GetClassName(self): 
+        # this function is mandatory if this is a full python workbench
+        return "Gui::PythonWorkbench"
 
 
     def Initialize(self):
         global dropDownCmdGroup
         import newModelCmd         # creates a new App::Part container called 'Model'
-        #import newSketchCmd        # creates a new Sketch in 'Model'
         import newDatumCmd         # creates a new LCS in 'Model'
         import newPartCmd          # creates a new App::Part container called 'Model'
-        #import newBodyCmd          # creates a new Body in 'Model
         import insertLinkCmd       # inserts an App::Link to a 'Model' in another file
         import placeLinkCmd        # places a linked part by snapping LCS (in the Part and in the Assembly)
         import placeDatumCmd       # places an LCS relative to an external file (creates a local attached copy)
@@ -93,8 +101,9 @@ class Assembly4Workbench(Workbench):
         #import newLinkArray        # creates a new array of App::Link
         #import makeLinkArray        # creates a new array of App::Link
         import gotoDocumentCmd     # opens the documentof the selected App::Link
+        import MeasureTask         # Measure tool in the Task panel
+        import makeBomCmd          # creates the parts list
         import HelpCmd             # shows a basic help window
-        #import showCommand         # shows a 
         
         # create the toolbars and menus, nearly empty, to decide about their position
         self.appendToolbar("Assembly",["Asm4_newModel"])
@@ -216,11 +225,12 @@ class Assembly4Workbench(Workbench):
                                 "Asm4_newBody", 
                                 "Part_Import",
                                 "Asm4_insertLink", 
-                                "Asm4_placeLink", 
-                                "Asm4_releaseAttachment", 
+                                "Asm4_makeBOM", 
                                 "Asm4_insertScrew", 
                                 "Asm4_insertNut", 
                                 "Asm4_insertWasher",
+                                "Asm4_placeLink", 
+                                "Asm4_releaseAttachment", 
                                 "Asm4_placeFastener", 
                                 "Asm4_newSketch", 
                                 "Asm4_newLCS", 
@@ -231,6 +241,7 @@ class Assembly4Workbench(Workbench):
                                 "Asm4_placeDatum", 
                                 "Asm4_importDatum", 
                                 #"Asm4_makeLinkArray",
+                                "Asm4_Measure", 
                                 "Asm4_addVariable", 
                                 "Asm4_Animate", 
                                 "Asm4_updateAssembly"]
@@ -240,6 +251,7 @@ class Assembly4Workbench(Workbench):
                                 "Asm4_newBody", 
                                 "Asm4_insertLink", 
                                 FastenersDropDown, 
+                                "Asm4_makeBOM", 
                                 "Separator",
                                 "Asm4_newSketch", 
                                 'Asm4_newLCS', 
@@ -254,6 +266,7 @@ class Assembly4Workbench(Workbench):
                                 "Asm4_placeDatum", 
                                 "Separator",
                                 #"Asm4_makeLinkArray",
+                                "Asm4_Measure", 
                                 "Asm4_addVariable", 
                                 "Asm4_Animate",
                                 "Asm4_updateAssembly"]
@@ -284,49 +297,6 @@ class Assembly4Workbench(Workbench):
                                 "Asm4_insertWasher",
                                 #"Asm4_makeLinkArray"
                                 ]
-        
-
-    """
-    +-----------------------------------------------+
-    |               helper functions                |
-    +-----------------------------------------------+
-    """
-    def checkWorkbench( self, workbench ):
-        # checks whether the specified workbench is installed
-        listWB = Gui.listWorkbenches()
-        hasWB = False
-        for wb in listWB.keys():
-            if wb == workbench:
-                hasWB = True
-        return hasWB
-
-
-    """
-    +-----------------------------------------------+
-    |          Standard necessary functions         |
-    +-----------------------------------------------+
-    """
-    def Activated(self):
-        """
-        from PySide import QtGui, QtCore
-        "This function is executed when the workbench is activated"
-        # Set the drop-down button for the Configurations in text mode 
-        # QtCore.Qt.ToolButtonTextOnly
-        # QtCore.Qt.ToolButtonTextBesideIcon
-        # QtCore.Qt.ToolButtonTextUnderIcon
-        # QtCore.Qt.ToolButtonIconOnly
-        for toolbar in Gui.getMainWindow().findChildren(QtGui.QToolBar):
-            if toolbar.objectName()=='Assembly 4':
-                for button in toolbar.children():
-                    if button.isWidgetType() and button.text()=='Configurations':
-                        button.setToolButtonStyle( QtCore.Qt.ToolButtonTextOnly )
-        """
-        return
-
-
-    def Deactivated(self):
-        "This function is executed when the workbench is deactivated"
-        return 
 
 
     def ContextMenu(self, recipient):
@@ -338,10 +308,20 @@ class Assembly4Workbench(Workbench):
         self.appendContextMenu( "Create", self.itemsCreateMenu ) # add commands to the context menu
         self.appendContextMenu( "", "Separator")
 
- 
-    def GetClassName(self): 
-        # this function is mandatory if this is a full python workbench
-        return "Gui::PythonWorkbench"
+
+    """
+    +-----------------------------------------------+
+    |               helper functions                |
+    +-----------------------------------------------+
+    """
+    def checkWorkbench( self, workbench ):
+        # checks whether the specified workbench (a 'string') is installed
+        listWB = Gui.listWorkbenches()
+        hasWB = False
+        for wb in listWB.keys():
+            if wb == workbench:
+                hasWB = True
+        return hasWB
 
     
     
---
+++ makeBomCmd.py
@@ -0,0 +1,342 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# 
+# makeBomCmd.py 
+#
+# parses the Asm4 Model tree and creates a list of parts
+
+
+
+import os
+
+from PySide import QtGui, QtCore
+import FreeCADGui as Gui
+import FreeCAD as App
+from FreeCAD import Console as FCC
+
+import libAsm4 as Asm4
+
+
+
+
+"""
+    +-----------------------------------------------+
+    |               Helper functions                |
+    +-----------------------------------------------+
+"""
+# Types of objects to import
+datumTypes = ['PartDesign::CoordinateSystem',
+                           'PartDesign::Plane',
+                           'PartDesign::Line',
+                           'PartDesign::Point']
+
+
+
+"""
+    +-----------------------------------------------+
+    |               prints a parts list             |
+    +-----------------------------------------------+
+def Partlist(object,level=0):
+    indent = '  '*level
+    # list the Variables
+    if object.Name=='Variables':
+        print(indent+'Variables:')
+        vars = object
+        for prop in vars.PropertiesList:
+            if vars.getGroupOfProperty(prop)=='Variables' :
+                propValue = vars.getPropertyByName(prop)
+                print(indent+'  '+prop+' = '+str(propValue))
+    # if its a link, look for the linked object
+    elif object.TypeId=='App::Link':
+        print(indent+object.Label+' -> '+object.LinkedObject.Document.Name+'#'+object.LinkedObject.Label)
+        Partlist(object.LinkedObject,level+1)
+    # everything else
+    else:
+        print(indent+object.Label+' ('+object.TypeId+')')
+        # if it's a part, look for sub-objects
+        if object.TypeId=='App::Part':
+            for objname in object.getSubObjects():
+                subobj = object.Document.getObject( objname[0:-1] )
+                Partlist(subobj,level+1)
+    return
+    
+    
+    
+    
+import FreeCAD,Draft
+import csv
+
+forbbox = ('PartDesign::Body', 'Part::Feature', 'Part::FeaturePython')
+
+def Partlist(object,level=0,tab=None):
+    indent = '  '*level
+    if tab == None:
+        tab = {}
+    if object.TypeId=='App::Link':
+        print(indent+object.Label+' -> '+object.LinkedObject.Document.Name+'#'+object.LinkedObject.Label+' => '+object.LinkedObject.FullName)
+        Partlist(object.LinkedObject,level+1,tab)
+    else:
+        print(indent+object.Label+' ('+object.TypeId+')')
+        if hasattr(object, 'Shape') and object.TypeId in forbbox:
+            if object.FullName in tab:
+                tab[object.FullName]["count"] = tab[object.FullName]["count"] + 1
+            else:
+                tab[object.FullName] = {}
+                tab[object.FullName]["var"] = {} 
+                tab[object.FullName]["count"] = 1
+
+            tab[object.FullName]['label'] = object.Label
+            tab[object.FullName]['fullname'] = object.FullName
+            tab[object.FullName]['Id'] = object.Label
+            bb=object.Shape.BoundBox
+            tab[object.FullName]['xlen'] = bb.XLength
+            tab[object.FullName]['ylen'] = bb.YLength
+            tab[object.FullName]['zlen'] = bb.ZLength
+            tab[object.FullName]['volume'] = str(object.Shape.Volume)
+
+            if hasattr(object, 'AttachedTo'):
+                tab[object.FullName]['attachedto'] = object.AttachedTo
+
+            print (indent+" => BBox: "+str(object.Shape.BoundBox))		
+            print (indent+" => Volume: "+str(object.Shape.Volume))		
+        if object.TypeId=='App::Part':
+            # look for Variables
+            if object.Document.getObject( 'Variables' ):
+                print(indent+'  Variables:')
+                vars = object.Document.getObject( 'Variables' )
+                for prop in vars.PropertiesList:
+                    if vars.getGroupOfProperty(prop)=='Variables' :
+                        propValue = vars.getPropertyByName(prop)
+                        print(indent+'    '+prop+' = '+str(propValue))
+                        if not object.FullName in tab:
+                            tab[object.FullName] = {}
+                            tab[object.FullName]['fullname'] = object.FullName
+                            tab[object.FullName]["var"] = {}
+                        tab[object.FullName]["var"][prop] = str(propValue) 
+            # look for sub-objects
+            for objname in object.getSubObjects():
+                subobj = object.Document.getObject( objname[0:-1] )
+                Partlist(subobj,level+1, tab)
+    return tab
+
+
+def dictoarr(tab): 
+    keys = {}
+    for obj in tab.keys():
+        if isinstance(tab[obj], dict):
+            for key in tab[obj].keys():
+                if isinstance(tab[obj][key], dict):
+                    for inner_key in tab[obj][key].keys():
+                        keys[key+"."+inner_key] = {};
+                        keys[key+"."+inner_key][0] = key;
+                        keys[key+"."+inner_key][1] = inner_key;
+                else:
+                        keys[key] = 1;
+    headings = sorted(keys.keys())
+
+    arr = [ headings ]
+    for obj in sorted(tab.keys()):
+        line = []
+        for head in headings:
+            value = ''
+            lookup = keys[head]
+            if isinstance(lookup, dict):
+                if lookup[0] in tab[obj] and lookup[1] in tab[obj][lookup[0]]: 	
+                    value = tab[obj][lookup[0]][lookup[1]]
+            else:
+                if head in tab[obj]:
+                    value = tab[obj][head]
+            line.append(value)
+        arr.append(line)	
+    return arr
+
+
+a = Partlist(FreeCAD.ActiveDocument.getObject("Model"), 0)
+print("\n\n")
+print(a)
+t = dictoarr(a)
+print("\n\n")
+print(t)
+
+with open('/tmp/table.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter="\t")
+    writer.writerows(t)
+    
+"""
+class makeBOM:
+    def __init__(self):
+        super(makeBOM,self).__init__()
+
+    def GetResources(self):
+        return {"MenuText": "Create Part List",
+                "ToolTip": "Create the Bom (Bill of Materials) of an Assembly4 Model",
+                "Pixmap" : os.path.join( Asm4.iconPath , 'Asm4_PartsList.svg')
+                }
+
+
+    def IsActive(self):
+        return self.checkModel()
+
+
+    def Activated(self):
+        self.UI = QtGui.QDialog()
+        # get the current active document to avoid errors if user changes tab
+        self.modelDoc = App.ActiveDocument
+        self.model = self.modelDoc.Model
+        self.drawUI()
+        self.UI.show()
+        self.BOM.clear()
+        self.PartsList = ''
+        self.listParts(self.model)
+        self.BOM.setPlainText(self.PartsList)
+
+
+    def listParts( self, obj, level=0 ):
+        indent = '\n'+'\t'*level
+        if obj.Document == self.modelDoc:
+            docName = ''
+        else:
+            docName = obj.Document.Name+'#'
+        #partBB = App.BoundBox()
+        # list the Variables
+        if obj.Name=='Variables':
+            #print(indent+'Variables:')
+            self.PartsList += indent+'Variables:'
+            for prop in obj.PropertiesList:
+                if obj.getGroupOfProperty(prop)=='Variables' :
+                    propValue = obj.getPropertyByName(prop)
+                    self.PartsList += indent+'\t'+prop+' = '+str(propValue)
+        # if it's part we look for sub-objects
+        elif obj.TypeId=='App::Part':
+            self.PartsList += indent +docName +obj.Label
+            for objname in obj.getSubObjects():
+                subobj = obj.Document.getObject( objname[0:-1] )
+                self.listParts( subobj, level+1 )
+        # if its a link, look for the linked object
+        elif obj.TypeId=='App::Link':
+            self.PartsList += indent+obj.Label+' -> '
+            self.listParts( obj.LinkedObject, level )
+        # if its a Body container we also add the document name and the size
+        elif obj.TypeId=='PartDesign::Body':
+            self.PartsList += indent +docName +obj.Label
+            if obj.Label2:
+                self.PartsList += ' ('+obj.Label2+')'
+            bb = obj.Shape.BoundBox
+            if abs(max(bb.XLength,bb.YLength,bb.ZLength)) < 1e+10:
+                Xsize = str(int((bb.XLength * 10)+0.099)/10)
+                Ysize = str(int((bb.YLength * 10)+0.099)/10)
+                Zsize = str(int((bb.ZLength * 10)+0.099)/10)
+                self.PartsList += ', Size: '+Xsize+' x '+Ysize+' x '+Zsize
+        # everything else except datum objects
+        elif obj.TypeId not in datumTypes:
+            self.PartsList += indent+obj.Label
+            if obj.Label2:
+                self.PartsList += ' ('+obj.Label2+')'
+            else:
+                self.PartsList += ' ('+obj.TypeId+')'
+            # if the object has a shape, add it at the end of the line
+            if hasattr(obj,'Shape') and obj.Shape.BoundBox.isValid():
+                bb = obj.Shape.BoundBox
+                if max(bb.XLength,bb.YLength,bb.ZLength) < 1e+10:
+                    Xsize = str(int((bb.XLength * 10)+0.099)/10)
+                    Ysize = str(int((bb.YLength * 10)+0.099)/10)
+                    Zsize = str(int((bb.ZLength * 10)+0.099)/10)
+                    self.PartsList += ', Size: '+Xsize+' x '+Ysize+' x '+Zsize
+        return
+
+
+    def onSave(self):
+        #pass
+        """Saves ASCII tree to user system file"""
+        _path = QtGui.QFileDialog.getSaveFileName()
+        if _path[0]:
+            save_file = QtCore.QFile(_path[0])
+            if save_file.open(QtCore.QFile.ReadWrite):
+                save_fileContent = QtCore.QTextStream(save_file)
+                save_fileContent << self.BOM
+                save_file.flush()
+                save_file.close()
+                self.BOM.setPlainText("Saved to file : " + _path[0])
+            else:
+                #FCC.PrintError("ERROR : Can't open file : "+ _path[0]+'\n')
+                self.BOM.setPlainText("ERROR : Can't open file : " + _path[0])
+        else:
+            self.BOM.setPlainText("ERROR : Can't open file : " + _path[0])
+        QtCore.QTimer.singleShot(3000, lambda:self.BOM.setPlainText(self.PartsList))
+        #self.UI.close()
+
+
+    def isReal( bb ):
+        # check if the BoundingBox is a real one
+        if bb.isValid() and abs(max(bb.XLength,bb.YLength,bb.ZLength)) < 1e+10:
+            return True
+        else:
+            return False
+
+
+    def checkModel(self):
+        # check whether there is already a Model in the document
+        # Returns True if there is an object called 'Model'
+        if App.ActiveDocument and App.ActiveDocument.getObject('Model') and App.ActiveDocument.Model.TypeId=='App::Part':
+            return(True)
+        else:
+            return(False)
+
+
+    def onCopy(self):
+        """Copies Parts List to clipboard"""
+        self.BOM.selectAll()
+        self.BOM.copy()
+        self.BOM.setPlainText("Copied BoM to clipboard")
+        QtCore.QTimer.singleShot(3000, lambda:self.BOM.setPlainText(self.PartsList))
+
+
+    def onOK(self):
+        self.UI.close()
+
+
+    """
+    +-----------------------------------------------+
+    |     defines the UI, only static elements      |
+    +-----------------------------------------------+
+    """
+    def drawUI(self):
+        # Our main window will be a QDialog
+        self.UI.setWindowTitle('Parts List / BOM')
+        self.UI.setWindowIcon( QtGui.QIcon( os.path.join( Asm4.iconPath , 'FreeCad.svg' ) ) )
+        self.UI.setWindowFlags( QtCore.Qt.WindowStaysOnTopHint )
+        self.UI.setModal(False)
+        # set main window widgets layout
+        self.mainLayout = QtGui.QVBoxLayout(self.UI)
+
+        # The list, is a plain text field
+        self.BOM = QtGui.QPlainTextEdit()
+        self.BOM.setMinimumSize(600,500)
+        self.BOM.setLineWrapMode(QtGui.QPlainTextEdit.NoWrap)
+        self.mainLayout.addWidget(self.BOM)
+
+        # the button row definition
+        self.buttonLayout = QtGui.QHBoxLayout()
+        self.buttonLayout.addStretch()
+        # Save button
+        self.CopyButton = QtGui.QPushButton('Copy')
+        self.buttonLayout.addWidget(self.CopyButton)
+        # Save button
+        #self.SaveButton = QtGui.QPushButton('Save')
+        #self.buttonLayout.addWidget(self.SaveButton)
+        # OK button
+        self.OkButton = QtGui.QPushButton('Close')
+        self.OkButton.setDefault(True)
+        self.buttonLayout.addWidget(self.OkButton)
+        self.mainLayout.addLayout(self.buttonLayout)
+
+        # finally, apply the layout to the main window
+        self.UI.setLayout(self.mainLayout)
+
+        # Actions
+        self.CopyButton.clicked.connect(self.onCopy)
+        #self.SaveButton.clicked.connect(self.onSave)
+        self.OkButton.clicked.connect(self.onOK)
+
+# add the command to the workbench
+Gui.addCommand( 'Asm4_makeBOM', makeBOM() )
