92284fbb9e9567da2d7a03f2df0305180cce4789
Alex <alex@4project.co.il>
2020-09-17 15:27:55 +0300

    Save and Restore logic

--- configurationEngine.py
+++ configurationEngine.py
@@ -8,6 +8,7 @@
 import FreeCADGui as Gui
 import FreeCAD as App
 import libAsm4 as Asm4
+import math
 
 HEADER_CELL             = 'A1'
 NAME_CELL               = 'A2'
@@ -50,12 +51,11 @@ class saveConfigurationCmd:
     """
     def Activated(self):
         #TODO: Ask for configuration name
-        doc = GetDocument('test')
+        doc = GetDocument('test', 'Configurations')
 
         model = Asm4.getModelSelected()
         if model:
-            for objName in model.getSubObjects():
-                SaveObject(doc, model.getSubObject(objName, 1))
+            SaveSubObjects(doc, model)
         else:
             SaveObject(doc, Asm4.getSelection())
 
@@ -83,12 +83,11 @@ class restoreConfigurationCmd:
     """
     def Activated(self):
         #TODO: Ask for configuration name
-        doc = GetDocument('test')
+        doc = GetDocument('test', 'Configurations')
 
         model = Asm4.getModelSelected()
         if model:
-            for objName in model.getSubObjects():
-                RestoreObject(doc, model.getSubObject(objName, 1))
+            RestoreSubObjects(doc, model)
         else:
             RestoreObject(doc, Asm4.getSelection())
 
@@ -98,23 +97,25 @@ class restoreConfigurationCmd:
     |                  storage engine               |
     +-----------------------------------------------+
 """
-def GetDocument(name):
-    # Get Configurations group where the spreadsheets will be saved
-    conf = App.ActiveDocument.getObject('Configurations')
-    if conf is None:
-        conf = App.ActiveDocument.addObject('App::DocumentObjectGroup', 'Configurations')
+def GetDocument(name, groupName = ''):
+    # Look for the specified group, or ActiveDocument if not specified
+    if groupName != '':
+        group = App.ActiveDocument.getObject(groupName)
+        if group is None:
+            group = App.ActiveDocument.addObject('App::DocumentObjectGroup', groupName)
+    else:
+        group = App.ActiveDocument
 
     # Get the needed document, create if not exist
-    doc = conf.getObject(name)
+    doc = group.getObject(name)
     if doc is None:
-        doc = conf.newObject('Spreadsheet::Sheet', name)
+        doc = group.newObject('Spreadsheet::Sheet', name)
         PrepareDocument(doc, name)
 
     return doc
 
 
 def PrepareDocument(doc, name):
-    print('Preparing: ' + name)
     doc.clearAll()
     doc.set(HEADER_CELL, 'This is an Assembly4 configuration file. Manual changes or deleletion of that file might break your assembly completely!')
     doc.set(NAME_CELL, name)
@@ -128,8 +129,22 @@ def PrepareDocument(doc, name):
     doc.set(ATTACHMENT_AXIS_Z_COL + headerRow, 'Axis_Z')
     doc.set(ATTACHMENT_ANGLE_COL + headerRow, 'Angle')
 
+
+def GetValidAlias(str):
+    # Spreadsheed doesn't like many characters in the alias, specifically the '.' that we need to separate sub-sub-links
+    # For now we will just remove all those characters in hope that there will be no duplication
+    badChars = '`~!@#$%^&*()-+=|\;:\'".,'
+    ret = ''
+    for char in str:
+        if char not in badChars:
+            ret = ret + char
+    # Should not have '_' at the beginning of the string...
+    ret = ret.strip('_')
+    return ret
+
+
 def GetObjectRow(doc, name):
-    cell = doc.getCellFromAlias(name)
+    cell = doc.getCellFromAlias(GetValidAlias(name))
     if cell:
         # leave only numbers in the cell string
         row = ''.join(i for i in cell if i.isdigit())
@@ -142,14 +157,27 @@ def GetObjectData(doc, name, col):
     return doc.get(str(col) + str(row))
 
 
-def SaveObject(doc, obj):
-    row = GetObjectRow(doc, obj.Name)
+def SaveSubObjects(doc, container, namePrefix=''):
+    print("Container: " + container.Name)
+    print("Prefix: " + namePrefix)
+    for objName in container.getSubObjects():
+        obj = container.getSubObject(objName, 1)
+        if obj.TypeId == 'App::Link':
+            SaveObject(doc, obj, namePrefix)
+
+
+def SaveObject(doc, obj, namePrefix=''):
+    linkedObjName = namePrefix + obj.Name + '.';
+
+    SaveSubObjects(doc, obj, linkedObjName)
+    print("Saving " + linkedObjName + " to " + doc.Name)
+    row = GetObjectRow(doc, linkedObjName)
     if row is None:
         doc.insertRows(OBJECTS_START_ROW, 1)
         row = OBJECTS_START_ROW
 
-    linkedObjName = Asm4.getLinkedObjectName(App.ActiveDocument.Name, App.ActiveDocument.Model.Name, obj.Name)
     doc.set(OBJECT_NAME_COL + row, linkedObjName)
+    doc.setAlias(OBJECT_NAME_COL + row, GetValidAlias(linkedObjName))
     attachment = obj.AttachmentOffset
     doc.set(ATTACHMENT_POS_X_COL + row, str(attachment.Base.x))
     doc.set(ATTACHMENT_POS_Y_COL + row, str(attachment.Base.y))
@@ -157,14 +185,44 @@ def SaveObject(doc, obj):
     doc.set(ATTACHMENT_AXIS_X_COL + row, str(attachment.Rotation.Axis.x))
     doc.set(ATTACHMENT_AXIS_Y_COL + row, str(attachment.Rotation.Axis.y))
     doc.set(ATTACHMENT_AXIS_Z_COL + row, str(attachment.Rotation.Axis.z))
-    doc.set(ATTACHMENT_ANGLE_COL + row, str(attachment.Rotation.Angle))
+    doc.set(ATTACHMENT_ANGLE_COL + row, str(math.degrees(attachment.Rotation.Angle)))
+    doc.recompute(True)
 
 
-def RestoreObject(doc, obj):
-    #TODO
-    todo = 1
+def RestoreSubObjects(doc, container, namePrefix=''):
+    for objName in container.getSubObjects():
+        obj = container.getSubObject(objName, 1)
+        if obj.TypeId == 'App::Link':
+            RestoreObject(doc, obj, namePrefix)
 
 
+def RestoreObject(doc, obj, namePrefix=''):
+    linkedObjName = namePrefix + obj.Name + '.';
+
+    RestoreSubObjects(doc, obj, namePrefix + obj.Name + '.')
+
+    print("Restoring " + linkedObjName + " from " + doc.Name)
+    row = GetObjectRow(doc, linkedObjName)
+    if row is None:
+        print('No data for object "' + linkedObjName + '." in configuration "' + doc.Name + '"')
+        return
+
+    x = doc.get(ATTACHMENT_POS_X_COL + row)
+    y = doc.get(ATTACHMENT_POS_Y_COL + row)
+    z = doc.get(ATTACHMENT_POS_Z_COL + row)
+    v1 = App.Vector(x, y, z)
+    print(v1)
+    x = doc.get(ATTACHMENT_AXIS_X_COL + row)
+    y = doc.get(ATTACHMENT_AXIS_Y_COL + row)
+    z = doc.get(ATTACHMENT_AXIS_Z_COL + row)
+    angle = doc.get(ATTACHMENT_ANGLE_COL + row)
+    v2 = App.Vector(x, y, z)
+    rotation = App.Rotation(v2, angle)
+    print(rotation)
+    placement = App.Placement(v1, rotation)
+    print(placement)
+    obj.AttachmentOffset = placement
+    obj.recompute()
 
 
 """
