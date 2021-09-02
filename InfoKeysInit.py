0678ac61d9b96f68a3301c564989e5e093a139c3
FarmingSoul <contact@farmingsoul.org>
2021-09-02 18:23:18 +0200

    infoPartCmd workable
    make workable infoPartUI and customisable configuration key and autoscript

---
+++ InfoKeysInit.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# coding: utf-8
+#
+# LGPL
+# Copyright HUBERT Zoltán
+#
+# libraries for FreeCAD's Assembly 4 workbench
+
+
+
+"""
+    +-----------------------------------------------+
+    |               customizable file               |
+    +-----------------------------------------------+
+"""
+
+# Entitled of the informations for your part
+
+partInfo =[     'PartID',                       \
+                'PartName',                     \
+                'PartDescription',              \
+                'PartSupplier']
