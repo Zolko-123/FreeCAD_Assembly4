826d3a184b858ed22fd2eee7fbf10ca8eed63255
FarmingSoul <contact@farmingsoul.org>
2021-09-02 18:39:49 +0200

    Update infoPartCmd.py

--- infoPartCmd.py
+++ infoPartCmd.py
@@ -102,11 +102,7 @@ class infoPartUI():
         self.form = self.base        
         iconFile = os.path.join( Asm4.iconPath , 'Asm4_PartInfo.svg')
         self.form.setWindowIcon(QtGui.QIcon( iconFile ))
-<<<<<<< HEAD
         self.form.setWindowTitle("Edit Part Information")
-=======
-        self.form.setWindowTitle("Edit Part Information (FS Mod)")
->>>>>>> 722e8b6304dc93bc3e9c3e1dd5f6da3c7780ed73
        
         # hey-ho, let's go
         self.part = checkPart()
@@ -128,7 +124,6 @@ class infoPartUI():
 
     def makePartInfo( self, reset=False ):
         # add the default part information
-<<<<<<< HEAD
         for info in InfoKeys.partInfo:
             try :
                 self.part
@@ -138,17 +133,6 @@ class infoPartUI():
             except AttributeError :
                 if self.TypeId == 'App::Part' :
                     #object part
-=======
-        for info in Asm4.partInfo:
-            try :
-                self.part
-                if not hasattr(self.part,info):
-                    print('object avec option part')
-                    self.part.addProperty( 'App::PropertyString', info, 'PartInfo' )
-            except AttributeError :
-                if self.TypeId == 'App::Part' :
-                    print ('object part')
->>>>>>> 722e8b6304dc93bc3e9c3e1dd5f6da3c7780ed73
                     self.addProperty( 'App::PropertyString', info, 'PartInfo' )    
         return
     
@@ -159,177 +143,11 @@ class infoPartUI():
                 if self.part.getTypeIdOfProperty(prop[0])=='App::PropertyString' :
                     text=self.infos[i].text()
                     setattr(self.part,prop[0],str(text))
-<<<<<<< HEAD
 
     # InfoDefault
     def infoDefault(self):
         autoInfo.infoDefault(self)    
 
-=======
-    
-    # InfoDefault
-    def infoDefault(self):
-        ###Macro pour automatisation des Informations liées à la pièce
-        ###et la compatibilité labête de l'atelier paysan [greg]
-        ###Nom de la pièce
-        ###Désignation
-        ###Longueur
-        ###Angle 1
-        ###Angle 2
-        ###Diamètre des troues
-
-        ###Création de la variable de la pièce
-        try :
-            self.TypeId
-            piece=self
-        except AttributeError:
-            piece=self.part
-        
-        print (piece.FullName)
-        for i in range(len(piece.Group)):
-            if piece.Group[i].TypeId == 'PartDesign::Pad' :
-                corp=piece.Group[i]
-                print (i, 'trouvé')
-            else :
-                print (i ,'cest pas' ,str(piece.Group[i].FullName))
-        try :
-            corp
-        except NameError :
-            print('recherche profonde')
-            for i in range(len(piece.Group)):
-                if piece.Group[i].TypeId == 'PartDesign::Body' :
-                    body=piece.Group[i]
-                    print (i, 'trouvé Body')
-                    for i in range(len(body.Group)):
-                        print(body.FullName)
-                        if body.Group[i].TypeId == 'PartDesign::Pad' :
-                            corp=body.Group[i]
-                            print (i, 'trouvé pad profond')
-                else :
-                    print ('la pièce : ' ,str(piece.Group[i].FullName), "n'est pas un profile standard")
-        try :
-            corp
-        except NameError :
-            print('y a pas moyen pour : ',piece.FullName ) 
-            setattr(piece,'Nom_de_la_piece',"'"+piece.Label)
-            return
-        print(corp.FullName)
-        barre=corp
-        profile=barre.Profile[0]
-        ###Recuperation des variables de la pièce
-        test=0
-        try :
-            ep=float(profile.getDatum('epaisseur'))
-            test+=1
-            temp=int(ep)
-            if ep==temp:
-                ep=int(ep)
-            ep=str(ep)
-        except NameError:
-            test=test
-        try :
-            lar= float(profile.getDatum('largeur'))
-            test+=1
-            temp=int(lar)
-            if lar==temp:
-                lar=int(lar)
-            lar=str(lar)
-        except NameError:
-            test=test
-        try :
-            hau= float(profile.getDatum('hauteur'))
-            test+=1
-            temp=int(hau)
-            if hau==temp:
-                hau=int(hau)
-            hau=str(hau)
-        except NameError:
-            test=test
-        try :
-            conge= float(profile.getDatum('conge'))
-            test+=1
-        except NameError:
-            test=test
-        try :
-            dia= float(profile.getDatum('diametre'))
-            test+=1
-            temp=int(dia)
-            if dia==temp:
-                dia=int(dia)
-            dia=str(dia)
-        except NameError:
-            test=test
-        
-        ### Déduction du profile
-
-        if test==1:
-            descri="fer rond Ø"+dia
-        if test==2:
-            try :
-                descri="Tube rond "+dia+" x "+ep
-            except NameError:
-                descri="étiré plat "+lar+" x "+ep
-        if test==3:
-            try:
-                if hau==lar:
-                    descri="Tube carré "+hau+" x "+ep
-                else:
-                    descri="Tube rectangulaire "+hau+" x "+lar+" x "+ep
-            except NameError:
-                descri="fer plat "+lar+" x "+ep
-        if test==0:
-            print("le profile n'a pas était déterminé : contrainte non nomées")
-            descri = "profile non déterminé"
-
-        ###Création de la variable de percage si elle existe
-        try:
-            troue=App.ActiveDocument.getObject('Hole')
-            sktroue=troue.OutList[0]
-            troue1= str(len(App.ActiveDocument.getObject(sktroue.Name).Geometry))+" x D : "+str(troue.Diameter)
-        except AttributeError:
-            troue1=""
-        try:
-            angle1=str(App.ActiveDocument.getObject('Groove').Angle.Value)
-        except AttributeError:
-            angle1=""
-        try:
-            angle2=str(App.ActiveDocument.getObject('Groove001').Angle.Value)
-        except AttributeError:
-            angle2=""
-
-        ###Recuperation des variables
-        nom="'"+piece.Label
-        longueur=float(barre.Length)
-        temp=int(longueur)
-        if longueur==temp:
-            longueur=int(longueur)
-        longueur=str(longueur)
-
-
-        ###Ecriture des parametre dans le fichier #PARTINFO# existant
-        setattr(piece,'Nom_de_la_piece',nom)
-        setattr(piece,'Reference_AP',descri)
-        setattr(piece,'Angle1',angle1)
-        setattr(piece,'Angle2',angle2)
-        setattr(piece,'percage',troue1)
-        setattr(piece,'longueur',longueur)
-
-
-        ###Affichage du résultat
-        check=nom+" - "+descri+" - "+longueur+" mm"
-        print (check)
-        
-        ###Actualisation
-        try :
-            self.getPartInfo()
-            print('actualisation')
-            for i,prop in enumerate(self.infoTable):
-                if self.part.getGroupOfProperty(prop[0])=='PartInfo' :
-                    if self.part.getTypeIdOfProperty(prop[0])=='App::PropertyString' :
-                        self.infos[i].setText(prop[1])
-        except AttributeError:
-            pass
->>>>>>> 722e8b6304dc93bc3e9c3e1dd5f6da3c7780ed73
     # close
     def finish(self):
         Gui.Control.closeDialog()
@@ -369,13 +187,8 @@ class infoPartUI():
         
         # Buttons
         self.buttonsLayout = QtGui.QHBoxLayout()
-<<<<<<< HEAD
         self.AddNew = QtGui.QPushButton('Add New Info')
         self.InfoDefault = QtGui.QPushButton('automatic info')
-=======
-        self.AddNew = QtGui.QPushButton('Ajouter une nouvelle info')
-        self.InfoDefault = QtGui.QPushButton('Info par default')
->>>>>>> 722e8b6304dc93bc3e9c3e1dd5f6da3c7780ed73
         self.buttonsLayout.addWidget(self.AddNew)
         self.buttonsLayout.addStretch()
         self.buttonsLayout.addWidget(self.InfoDefault)
