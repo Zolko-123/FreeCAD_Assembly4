af8ad1ca2de30af53df45cf9dad3dbad29ea5c51
FarmingSoul <contact@farmingsoul.org>
2021-09-18 23:53:58 +0200

    french translation start
    if it's possible to remake asm4.ts i can make more

---
+++ Resources/translations/asm4_fr.ts
@@ -0,0 +1,238 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>App::Property</name>
+    <message>
+        <location filename="../AnimationLib.py" line="737"/>
+        <source>The hintfield for the animation dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Asm4</name>
+    <message>
+        <location filename="../InitGui.py" line="57"/>
+        <source>Activating Assembly4 WorkBench</source>
+        <translation>Activation de l&apos;atelier Assembly4</translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="75"/>
+        <source>Leaving Assembly4 WorkBench</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="95"/>
+        <source>Initializing Assembly4 workbench</source>
+        <translation>Initialisation de l&apos;atelier Assembly4</translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="294"/>
+        <source>Assembly</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="160"/>
+        <source>Selection Filter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="165"/>
+        <source>done</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="295"/>
+        <source>Create</source>
+        <translation>Créer</translation>
+    </message>
+</context>
+<context>
+    <name>Asm4_Animate</name>
+    <message>
+        <location filename="../AnimationLib.py" line="91"/>
+        <source>Variable name invalid</source>
+        <translation>Nom de variable invalide</translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="92"/>
+        <source>The selected variable name &quot;{0}&quot; is not valid. Please select an existing variable.</source>
+        <translation>Le nom de variable selectionée &quot;{0}&quot; n&apos;est pas valide. S&apos;il vous plait choisissez une varible existante.</translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="464"/>
+        <source>Animate Assembly</source>
+        <translation>Animer l&apos;assemblage</translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="162"/>
+        <source>Select Variable (only float)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="506"/>
+        <source>Variable</source>
+        <translation>Variable</translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="480"/>
+        <source>Range Begin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="485"/>
+        <source>Range End</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="491"/>
+        <source>Step Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="499"/>
+        <source>Sleep (s)</source>
+        <translation>Pause (s)</translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="507"/>
+        <source>Current Value:</source>
+        <translation>Valeur actuel :</translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="519"/>
+        <source>Begin</source>
+        <translation>Début</translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="520"/>
+        <source>End</source>
+        <translation>Fin</translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="521"/>
+        <source>The last reachable variable value with the given stepping. Flagged red in case this is not equal to the intended value. The last step of the animation will be reduced to stay inside the configured limits.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="535"/>
+        <source>Force GUI to update on every step.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="536"/>
+        <source>Force-render every step</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="541"/>
+        <source>Infinite Loop</source>
+        <translation>Boucle infinit</translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="542"/>
+        <source>Loop</source>
+        <translation>Boucle</translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="547"/>
+        <source>Back-and-forth pendulum</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="548"/>
+        <source>Pendulum</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="561"/>
+        <source>Close</source>
+        <translation>Fermer</translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="565"/>
+        <source>Export</source>
+        <translation>Exporter</translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="569"/>
+        <source>Stop</source>
+        <translation>Stop</translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="574"/>
+        <source>Run</source>
+        <translation>Démarrer</translation>
+    </message>
+    <message>
+        <location filename="../AnimationLib.py" line="282"/>
+        <source>Unknown State/Transition</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Asm4_Help</name>
+    <message>
+        <location filename="../HelpCmd.py" line="36"/>
+        <source>Help for Assembly4</source>
+        <translation>Aide pour Assembly4</translation>
+    </message>
+    <message>
+        <location filename="../HelpCmd.py" line="37"/>
+        <source>Show basic usage for FreeCAD and Assembly4</source>
+        <translation>Voir l&apos;utilisationbasique pour FreeCAD et Assembly4</translation>
+    </message>
+</context>
+<context>
+    <name>Asm4_gotoDocument</name>
+    <message>
+        <location filename="../gotoDocumentCmd.py" line="32"/>
+        <source>Open Document</source>
+        <translation>Ouvrir le document</translation>
+    </message>
+    <message>
+        <location filename="../gotoDocumentCmd.py" line="33"/>
+        <source>Activates the document of the selected linked part</source>
+        <translation>Activer le document de la linked part sélectionnée</translation>
+    </message>
+</context>
+<context>
+    <name>Asm4_hideLcs</name>
+    <message>
+        <location filename="../showHideLcsCmd.py" line="67"/>
+        <source>Hide LCS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../showHideLcsCmd.py" line="68"/>
+        <source>Hide LCS and Datums of selected part and its children</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Asm4_showLcs</name>
+    <message>
+        <location filename="../showHideLcsCmd.py" line="29"/>
+        <source>Show LCS</source>
+        <translation>Voir LCS</translation>
+    </message>
+    <message>
+        <location filename="../showHideLcsCmd.py" line="30"/>
+        <source>Show LCS and Datums of selected part and its children</source>
+        <translation>Voir LCS et les données de la part selectionné et ses enfants</translation>
+    </message>
+</context>
+<context>
+    <name>Workbench</name>
+    <message>
+        <location filename="../InitGui.py" line="145"/>
+        <source>&amp;Assembly</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InitGui.py" line="151"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Aide</translation>
+    </message>
+</context>
+</TS>
