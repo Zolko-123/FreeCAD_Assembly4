a6ae58fc1e79b83b489aee9f2a172e45bde2b331
FarmingSoul <contact@farmingsoul.org>
2021-09-18 12:05:34 +0200

    clean and documenting code
    reload infoScriptInit but it's internal script

---
+++ InfoScriptInit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# coding: utf-8
+#
+# LGPL
+#
+# infoScriptInit.py
+
+
+#customization file for automatic info partinfoUI
+
+
+def infoDefault(self):
+    
+    print ('wait for the autofilling script')
