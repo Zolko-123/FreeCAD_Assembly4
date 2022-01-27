89734516f7d20e92e855cbbb2896a688f723aeec
Zoltan Hubert <hubertz@# ipag-9104>
2022-01-27 08:46:45 +0100

    added package.xml

--- Init.py
+++ Init.py
@@ -23,9 +23,13 @@
 #  
 ###################################################################################
 
+import FreeCAD
+
 import Asm4_locator
 Asm4_path = os.path.dirname( Asm4_locator.__file__ )
 # Assembly4 version info
+
+# with file VERSION
 versionPath = os.path.join( Asm4_path, 'VERSION' )
 versionFile = open(versionPath,"r")
 # read second line
@@ -33,5 +37,13 @@ version = versionFile.readlines()[1]
 versionFile.close()
 # remove trailing newline
 Asm4_version = version[:-1]
+
+'''
+# with file package.xml
+packageFile  = os.path.join( Asm4_path, 'package.xml' )
+metadata     = FreeCAD.Metadata(packageFile)
+Asm4_date    = metadata.Date
+Asm4_version = metadata.Version
+'''
 
 print("Assembly4 workbench ("+Asm4_version+") loaded")
