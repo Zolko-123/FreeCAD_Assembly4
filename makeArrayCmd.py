9fcbd112bc1e9600eccab2716b992b1c47413f92
Jolbas <39026960+Jolbas@users.noreply.github.com>
2022-10-15 17:36:58 +0200

    Corrected string delimiters

--- makeArrayCmd.py
+++ makeArrayCmd.py
@@ -21,26 +21,26 @@ from Asm4_objects import (
 )
 
 
-"""
+'''
     +-----------------------------------------------+
     |    an expression link array class and command    |
     +-----------------------------------------------+
-"""
+'''
 
 class makeExpressionArray:
-    """Create a array of the selected object where the placement of each element
-       is calculated using expressions and an ElementIndex property"
+    '''Create a array of the selected object where the placement of each element
+       is calculated using expressions and an ElementIndex property'
        Select an object to array and optionally an Axis that transformation will be related to.
-       Without axis the transformations relates to the objects origin Z axis"""
+       Without axis the transformations relates to the objects origin Z axis'''
     def __init__(self):
         pass
 
     def GetResources(self):
-        iconFile = os.path.join(Asm4.iconPath, "Asm4_ExpressionArray.svg")
+        iconFile = os.path.join(Asm4.iconPath, 'Asm4_ExpressionArray.svg')
         return {
-            "MenuText": "Create a expression Array",
-            "ToolTip": self.__doc__,
-            "Pixmap": iconFile,
+            'MenuText': 'Create a expression Array',
+            'ToolTip': self.__doc__,
+            'Pixmap': iconFile,
         }
 
     def IsActive(self):
@@ -57,33 +57,33 @@ class makeExpressionArray:
 
             Gui.Selection.clearSelection()
             Gui.Selection.addSelection(
-                objParent.Document.Name, objParent.Name, array.Name + "."
+                objParent.Document.Name, objParent.Name, array.Name + '.'
             )
 
     def Activated(self):
         self._Activated(ExpressionArray)
 
 
-"""
+'''
     +-----------------------------------------------+
     |    a circular link array class and command    |
     +-----------------------------------------------+
-"""
+'''
 
 class makeCircularArray(makeExpressionArray):
-    """Create a circular (polar) array of the selected object
+    '''Create a circular (polar) array of the selected object
        Select first an object and then the axis
        The axis can be either a datum Axis, an Origin axis or the axis of an LCS
-       but it must be in the same container as the selected object"""
+       but it must be in the same container as the selected object'''
     def __init__(self):
         pass
 
     def GetResources(self):
-        iconFile = os.path.join(Asm4.iconPath, "Asm4_PolarArray.svg")
+        iconFile = os.path.join(Asm4.iconPath, 'Asm4_PolarArray.svg')
         return {
-            "MenuText": "Create a circular Array",
-            "ToolTip": self.__doc__,
-            "Pixmap": iconFile,
+            'MenuText': 'Create a circular Array',
+            'ToolTip': self.__doc__,
+            'Pixmap': iconFile,
         }
 
     def IsActive(self):
@@ -94,26 +94,26 @@ class makeCircularArray(makeExpressionArray):
         self._Activated(PolarArray)
 
 
-"""
+'''
     +-----------------------------------------------+
     |    a linear link array class and command    |
     +-----------------------------------------------+
-"""
+'''
 
 class makeLinearArray(makeExpressionArray):
-    """Create a linear array of the selected object
+    '''Create a linear array of the selected object
         Select first an object and then an axis for the direction
         The axis can be either a datum Axis, an Origin axis or the axis of an LCS
-        but it must be in the same container as the selected object"""
+        but it must be in the same container as the selected object'''
     def __init__(self):
         pass
 
     def GetResources(self):
-        iconFile = os.path.join(Asm4.iconPath, "Asm4_LinkArray.svg")
+        iconFile = os.path.join(Asm4.iconPath, 'Asm4_LinkArray.svg')
         return {
-            "MenuText": "Create a linear Array",
-            "ToolTip": self.__doc__,
-            "Pixmap": iconFile,
+            'MenuText': 'Create a linear Array',
+            'ToolTip': self.__doc__,
+            'Pixmap': iconFile,
         }
 
     def IsActive(self):
@@ -124,7 +124,7 @@ class makeLinearArray(makeExpressionArray):
         self._Activated(LinearArray)
 
 
-"""
+'''
     +-----------------------------------------------+
     |                 test functions                |
     +-----------------------------------------------+
@@ -145,14 +145,14 @@ array.PlacementList = pls
 import makeArrayCmd
 array = makeArrayCmd.makeCircularArray(obj,20)
 
-"""
+'''
 
 
 def makeMyLink(obj):
     # addObject() API is extended to accept extra parameters in order to
     # let the python object override the type of C++ view provider
     link = obj.Document.addObject(
-        "App::FeaturePython", "LinkArray", LinkArray(), None, True
+        'App::FeaturePython', 'LinkArray', LinkArray(), None, True
     )
     # ViewProviderArray(link.ViewObject)
     link.setLink(obj)
@@ -161,7 +161,7 @@ def makeMyLink(obj):
 
 def makeArray(obj, count):
     array = obj.Document.addObject(
-        "App::FeaturePython", "LinkArray", LinkArray(), None, True
+        'App::FeaturePython', 'LinkArray', LinkArray(), None, True
     )
     ViewProviderArray(array.ViewObject)
     array.setLink(obj)
@@ -170,6 +170,6 @@ def makeArray(obj, count):
 
 
 # add the command to the workbench
-Gui.addCommand("Asm4_linearArray", makeLinearArray())
-Gui.addCommand("Asm4_circularArray", makeCircularArray())
-Gui.addCommand("Asm4_expressionArray", makeExpressionArray())
+Gui.addCommand('Asm4_linearArray', makeLinearArray())
+Gui.addCommand('Asm4_circularArray', makeCircularArray())
+Gui.addCommand('Asm4_expressionArray', makeExpressionArray())
