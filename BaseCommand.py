8cd58879a277132d2a9301362539765f40eac4cf
hasecilu <hasecilu@tuta.io>
2024-02-23 15:12:24 -0600

    Add `BaseCommand` class
    This class is used to assign the resources from `self.pixmap`,
    `self.menutext`, `self.tooltip` instance variables.
    
    Variables are assigned on `__init__()` methods.
    
    The code is more readable than previous dictionary returns.

---
+++ BaseCommand.py
@@ -0,0 +1,23 @@
+import os
+
+import FreeCAD
+import FreeCADGui as Gui
+
+from TranslateUtils import translate
+
+class BaseCommand(object):
+    """Base class to prepare all the commands."""
+
+    def __init__(self):
+        pass
+
+    def Activated(self):
+        pass
+
+    def GetResources(self):
+        return {
+            "Pixmap": self.pixmap,
+            "MenuText": self.menutext,
+            "ToolTip": self.tooltip,
+        }
+
