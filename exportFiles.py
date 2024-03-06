7f1a70b96a0cbf7e2dbf1190fc2a54ab85222ca4
Zolko-123 <zoltan.hubert@zzaero.com>
2024-03-06 23:16:32 +0100

    better command name for Tree2Ascii

--- exportFiles.py
+++ exportFiles.py
@@ -43,8 +43,8 @@ except ImportError:
 class listLinkedFiles():
 
     def GetResources(self):
-        menutext = "Tree of Linked Files"
-        tooltip  = "<p>Show the hierarchical tree structure of parts in the selected container. "
+        menutext = "Structure tree of the assembly"
+        tooltip  = "<p>Show the hierarchical tree structure of parts and sub-assemblies in the assembly. "
         tooltip += "The tree is displayed with ASCII art</p>"
         tooltip += "<p><b>Usage</b>: select an entity and click the command</p>"
         iconFile = os.path.join(Asm4.iconPath, 'Asm4_List_Liked_Files_Tree.svg')
